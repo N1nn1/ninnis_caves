951feb0dc56bcd825a37fc95e65d0e5741cb77df
0rc1nus <mitchellctlui@gmail.com>
2023-08-18 21:45:23 +0800

    Shorter

--- src/main/java/com/ninni/ninnis_caves/world/gen/features/StoneStripFeature.java
+++ src/main/java/com/ninni/ninnis_caves/world/gen/features/StoneStripFeature.java
@@ -28,7 +28,7 @@ public class StoneStripFeature extends Feature<StoneStripConfig> {
         fastNoise.SetFractalType(FastNoise.FractalType.RigidMulti);
         fastNoise.SetNoiseType(FastNoise.NoiseType.SimplexFractal);
         fastNoise.SetFractalOctaves(1);
-        fastNoise.SetFrequency(0.015F);
+        fastNoise.SetFrequency(0.009F);
         fastNoise.SetFractalLacunarity(0.2F);
         int range = 16;
         int yRange = range / 2;
@@ -37,7 +37,9 @@ public class StoneStripFeature extends Feature<StoneStripConfig> {
             for (int z = 0; z < range; z++) {
                 for (int y = -yRange; y < yRange; y++) {
                     mutableBlockPos.set(blockPos.getX() + x, blockPos.getY() + y, blockPos.getZ() + z);
-                    double noise = fastNoise.GetNoise(mutableBlockPos.getX(), mutableBlockPos.getY(), mutableBlockPos.getZ());
+                    int y1 = mutableBlockPos.getY();
+                    y1 /= 0.4D;
+                    double noise = fastNoise.GetNoise(mutableBlockPos.getX(), y1, mutableBlockPos.getZ());
                     if (noise > 0.95D) {
                         BlockState blockState = world.getBlockState(mutableBlockPos);
                         boolean flag = !blockState.is(BlockTags.GEODE_INVALID_BLOCKS) && !blockState.isAir() && blockState.is(BlockTags.BASE_STONE_OVERWORLD) && !(blockState.is(Blocks.DEEPSLATE) || blockState.is(Blocks.TUFF));
