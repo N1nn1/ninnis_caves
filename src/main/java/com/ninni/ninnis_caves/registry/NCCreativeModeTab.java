1286e4bd25e30b172066871f29defe0fb675bb26
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2023-08-18 16:23:16 +0200

    Limestone in the creative inventory

--- src/main/java/com/ninni/ninnis_caves/registry/NCCreativeModeTab.java
+++ src/main/java/com/ninni/ninnis_caves/registry/NCCreativeModeTab.java
@@ -2,11 +2,14 @@ package com.ninni.ninnis_caves.registry;
 
 import com.ninni.ninnis_caves.NinnisCaves;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 
 import static com.ninni.ninnis_caves.registry.NCItems.*;
 
@@ -29,6 +32,37 @@ public class NCCreativeModeTab {
 
             }).build()
     );
+
+    static {
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(entries -> {
+            entries.addBefore(Items.GRANITE,
+                    LIMESTONE,
+                    LIMESTONE_STAIRS,
+                    LIMESTONE_SLAB,
+                    LIMESTONE_WALL,
+                    LIMESTONE_BRICKS,
+                    CRACKED_LIMESTONE_BRICKS,
+                    LIMESTONE_BRICK_STAIRS,
+                    LIMESTONE_BRICK_SLAB,
+                    LIMESTONE_BRICK_WALL,
+                    LIMESTONE_BRICKS
+            );
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(entries -> {
+            entries.addBefore(Items.GRANITE, LIMESTONE);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COLORED_BLOCKS).register(entries -> {
+            entries.addAfter(Items.GLASS, REINFORCED_GLASS);
+            entries.addAfter(Items.GLASS_PANE, REINFORCED_GLASS_PANE);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries -> {
+            entries.addBefore(Items.TINTED_GLASS, REINFORCED_GLASS, REINFORCED_GLASS_PANE);
+        });
+    }
 
     private static CreativeModeTab register(String id, CreativeModeTab tab) {
         return Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, new ResourceLocation(NinnisCaves.MOD_ID, id), tab);
